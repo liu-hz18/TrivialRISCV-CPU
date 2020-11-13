e4b17c95346533f055ee7cff0fcb4ba087f0987e
liu-hz18 <1456437967@qq.com>
2020-11-14 00:45:58 +0800

    feat: logn method of CTZ

--- thinpad_top.srcs/sources_1/new/alu.v
+++ thinpad_top.srcs/sources_1/new/alu.v
@@ -18,6 +18,8 @@ reg zf;
 reg sf;
 reg vf;
 
+wire[`RegBus] temp = src_a & (-src_a);
+
 always @(*) begin
     if (rst) begin
         result = `ZERO_WORD;
@@ -56,102 +58,7 @@ always @(*) begin
             result = ( $signed(src_a) < $signed(src_b) ) ? src_a : src_b;
         end
         `ALU_OP_CTZ: begin
-            if (~src_a[0]) begin
-                result = result + 1;
-                if (~src_a[1]) begin
-                    result = result + 1;
-                    if (~src_a[2]) begin
-                        result = result + 1;
-                        if (~src_a[3]) begin
-                            result = result + 1;
-                            if (~src_a[4]) begin
-                                result = result + 1;
-                                if (~src_a[5]) begin
-                                    result = result + 1;
-                                    if (~src_a[6]) begin
-                                        result = result + 1;
-                                        if (~src_a[7]) begin
-                                            result = result + 1;
-                                            if (~src_a[8]) begin
-                                                result = result + 1;
-                                                if (~src_a[9]) begin
-                                                    result = result + 1;
-                                                    if (~src_a[10]) begin
-                                                        result = result + 1;
-                                                        if (~src_a[11]) begin
-                                                            result = result + 1;
-                                                            if (~src_a[12]) begin
-                                                                result = result + 1;
-                                                                if (~src_a[13]) begin
-                                                                    result = result + 1;
-                                                                    if (~src_a[14]) begin
-                                                                        result = result + 1;
-                                                                        if (~src_a[15]) begin
-                                                                            result = result + 1;
-                                                                            if (~src_a[16]) begin
-                                                                                result = result + 1;
-                                                                                if (~src_a[17]) begin
-                                                                                    result = result + 1;
-                                                                                    if (~src_a[18]) begin
-                                                                                        result = result + 1;
-                                                                                        if (~src_a[19]) begin
-                                                                                            result = result + 1;
-                                                                                            if (~src_a[20]) begin
-                                                                                                result = result + 1;
-                                                                                                if (~src_a[21]) begin
-                                                                                                    result = result + 1;
-                                                                                                    if (~src_a[22]) begin
-                                                                                                        result = result + 1;
-                                                                                                        if (~src_a[23]) begin
-                                                                                                            result = result + 1;
-                                                                                                            if (~src_a[24]) begin
-                                                                                                                result = result + 1;
-                                                                                                                if (~src_a[25]) begin
-                                                                                                                    result = result + 1;
-                                                                                                                    if (~src_a[26]) begin
-                                                                                                                        result = result + 1;
-                                                                                                                        if (~src_a[27]) begin
-                                                                                                                            result = result + 1;
-                                                                                                                            if (~src_a[28]) begin
-                                                                                                                                result = result + 1;
-                                                                                                                                if (~src_a[29]) begin
-                                                                                                                                    result = result + 1;
-                                                                                                                                    if(~src_a[30]) begin
-                                                                                                                                        result =result + 1;
-                                                                                                                                        if(~src_a[31]) begin
-                                                                                                                                            result =result + 1; 
-                                                                                                                                        end
-                                                                                                                                    end
-                                                                                                                                end
-                                                                                                                            end
-                                                                                                                        end
-                                                                                                                    end
-                                                                                                                end
-                                                                                                            end
-                                                                                                        end
-                                                                                                    end
-                                                                                                end
-                                                                                            end
-                                                                                        end
-                                                                                    end
-                                                                                end
-                                                                            end
-                                                                        end
-                                                                    end
-                                                                end
-                                                            end
-                                                        end
-                                                    end
-                                                end
-                                            end
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
+            result = {27'b0, (temp&32'h0000_ffff) == 0, (temp&32'h00ff_00ff) == 0, (temp&32'h0f0f_0f0f) == 0, (temp&32'h3333_3333) == 0, (temp&32'h5555_5555) == 0} + (temp == 0);
         end
         `ALU_OP_SBCLR: begin
             result = src_a & ~(32'h0000_0001 << (src_b & 31));
